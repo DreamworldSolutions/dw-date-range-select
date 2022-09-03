2cfcac703e276957a2df3db688e37dc6351b531f
Hiten Bhayani <hiten@hisab.co>
2022-09-03 13:06:00 +0530

    feat: START Implement Unit test-cases of valueProviders

---
+++ test/value-providers-factory.js
@@ -0,0 +1,17 @@
+describe("value-provider-factory", function () {
+  describe("#lastNMonths(n)", function () {
+    it("returns a function", function () {});
+  });
+  describe("#thisQuarter(fyStartsFrom)", function () {
+    it("returns a function", function () {});
+  });
+  describe("#lastQuarter(fyStartsFrom)", function () {
+    it("returns a function", function () {});
+  });
+  describe("#thisFinancialYear(startsFrom)", function () {
+    it("returns a function", function () {});
+  });
+  describe("#lastFinancialYear(startsFrom)", function () {
+    it("returns a function", function () {});
+  });
+});
---
+++ test/value-providers.js
@@ -0,0 +1,13 @@
+describe("value-provider", function () {
+  describe("#all()", function () {
+    it("returns empty object", function () {});
+  });
+
+  describe("#thisMonth()", function () {
+    it("returns this month", function () {});
+  });
+
+  describe("#lastMonth()", function () {
+    it("returns last month", function () {});
+  });
+});
