99e27f92fc87ee83ad194fad0856b291660c9421
Hiten Bhayani <hiten@hisab.co>
2022-09-03 13:02:52 +0530

    feat: START Add prototypes of valueProviders with JSDocs

---
+++ value-provider-factory.js
@@ -0,0 +1,36 @@
+export const _setCurrentDate = (timestamp) => {};
+
+/**
+ * factory function (valueProvider)
+ * returns a function (which acts as valueProvider and returns last N months start and end date)
+ * @param {Number} n is positive integer
+ */
+export const lastMonths = (n) => {};
+
+/**
+ * factory function (valueProvider)
+ * returns a function (which acts as valueProvider and returns this Quarter start and end date)
+ * @param {String} fyStartsFrom is String in the format “dd/mm”.
+ */
+export const thisQuarter = (fyStartsFrom) => {};
+
+/**
+ * factory function (valueProvider)
+ * returns a function (which acts as valueProvider and returns last Quarter start and end date)
+ * @param {String} fyStartsFrom is String in the format “dd/mm”.
+ */
+export const lastQuarter = (fyStartsFrom) => {};
+
+/**
+ * factory function (valueProvider)
+ * returns a function (which acts as valueProvider and returns this financian year start and end date)
+ * @param {String} startsFrom is String in the format “dd/mm”.
+ */
+export const thisFinancialYear = (startsFrom) => {};
+
+/**
+ * factory function (valueProvider)
+ * returns a function (which acts as valueProvider and returns last financian year start and end date)
+ * @param {String} startsFrom is String in the format “dd/mm”.
+ */
+export const lastFinancialYear = (startsFrom) => {};
---
+++ value-provider.js
@@ -0,0 +1,17 @@
+export const _setCurrentDate = (timestamp) => {};
+
+/**
+ * When duration is all
+ * @returns {Object} undefined
+ */
+export const all = () => {};
+
+/**
+ * returns an object whose start & end is the current month’s first & last date.
+ */
+export const thisMonth = () => {};
+
+/**
+ * returns an object whose start & end is the last month’s first & last date.
+ */
+export const lastMonth = () => {};
