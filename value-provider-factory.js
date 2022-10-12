a7f44b5305fe46ed8cf7ffe1e4d9d453895e21ce
Hiten Bhayani <99726733+hitendreamworld@users.noreply.github.com>
2022-10-12 12:56:07 +0530

    feat: LitElement demo page setup [ci skip] (#6)
    * feat: LitElement demo page setup [ci skip]
    
    * feat: dw-date-range-select LitElement implementation [ci skip] (#7)
    
    * feat: STRAT dw-date-range-select LitElement implementation
    
    * feat: update demo page
    
    * feat: value select when firstUpdate call
    
    * feat: new value providers lastNthMonth and beforeNDays (#8)
    
    * feat: update circleci config file

--- value-provider-factory.js
+++ value-provider-factory.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+import moment from "moment/dist/moment";
 
 const DATE_FORMAT = "YYYY-MM-DD";
 var currentDate;
@@ -91,6 +91,34 @@ export const lastFinancialYear = (startsFrom) => {
         .endOf("month")
         .subtract(1, "years")
         .format(DATE_FORMAT),
+    };
+  };
+};
+
+/**
+ * Computed last Nth Month start and end date (N is a positive integer)
+ * @param {Number} n last nth month
+ * @returns {Function} function (which acts as valueProvider and returns last Nth Month start and end date)
+ */
+export const lastNthMonth = (n) => {
+  return () => {
+    return {
+      start: moment(currentDate).subtract(n, "months").startOf("month").format(DATE_FORMAT),
+      end: moment(currentDate).subtract(n, "months").endOf("month").format(DATE_FORMAT),
+    };
+  };
+};
+
+/**
+ * only the end date is provided.
+ * If n is not a positive integer then return the current day as the end date and throw a warning log.
+ * @param {*} n last n days
+ * @returns {Function} function (which acts as valueProvider and returns before N days (from today)
+ */
+export const beforeNDays = (n) => {
+  return () => {
+    return {
+      end: moment(currentDate).subtract(n, "days").format(DATE_FORMAT),
     };
   };
 };
