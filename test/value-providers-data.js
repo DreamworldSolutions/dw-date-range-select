39a155c60130ec2661fea8bc0c111960449fc4a5
Hiten Bhayani <99726733+hitendreamworld@users.noreply.github.com>
2022-10-12 12:59:51 +0530

    feat: Implement Unit test-cases of valueProviders (#3)
    * feat: Implement Unit test-cases of valueProviders
    
    * feat: Implement valueProviders and valueProvidersFactory (#4)
    
    * feat: START Implement valueProviders and valueProvidersFactory
    
    * feat: LitElement prototypes with JSDoc (#5)

---
+++ test/value-providers-data.js
@@ -0,0 +1,45 @@
+export const currentDate = new Date("Oct 19 2021").getTime();
+
+export const thisMonthData = {
+  start: "2021-10-01",
+  end: "2021-10-31",
+};
+
+export const lastMonthData = {
+  start: "2021-09-01",
+  end: "2021-09-30",
+};
+
+export const lastNMonthisData = {
+  start: "2021-06-01",
+  end: "2021-09-30",
+};
+
+export const thisQuarterData = {
+  start: "2021-10-01",
+  end: "2021-12-31",
+};
+
+export const lastQuarterData = {
+  start: "2021-07-01",
+  end: "2021-09-30",
+};
+
+export const thisFinancialYearData = {
+  start: "2021-04-01",
+  end: "2022-03-31",
+};
+
+export const lastFinancialYearData = {
+  start: "2020-04-01",
+  end: "2021-03-31",
+};
+
+export const lastNthMonthData = {
+  start: "2021-08-01",
+  end: "2021-08-31",
+};
+
+export const beforeNDaysData = {
+  end: "2021-09-04",
+};
--- value-provider-factory.js
+++ value-provider-factory.js
@@ -1,36 +1,124 @@
-export const _setCurrentDate = (timestamp) => {};
+import moment from "moment/dist/moment";
+
+const DATE_FORMAT = "YYYY-MM-DD";
+var currentDate;
+export const _setCurrentDate = (timestamp) => {
+  currentDate = timestamp;
+};
 
 /**
  * factory function (valueProvider)
  * returns a function (which acts as valueProvider and returns last N months start and end date)
  * @param {Number} n is positive integer
  */
-export const lastMonths = (n) => {};
+export const lastNMonths = (n) => {
+  return () => {
+    return {
+      start: moment(currentDate).subtract(n, "months").startOf("month").format(DATE_FORMAT),
+      end: moment(currentDate).subtract(1, "months").endOf("month").format(DATE_FORMAT),
+    };
+  };
+};
 
 /**
  * factory function (valueProvider)
  * returns a function (which acts as valueProvider and returns this Quarter start and end date)
  * @param {String} fyStartsFrom is String in the format “dd/mm”.
  */
-export const thisQuarter = (fyStartsFrom) => {};
+export const thisQuarter = (fyStartsFrom) => {
+  return () => {
+    return {
+      start: moment(currentDate).startOf("quarter").format(DATE_FORMAT),
+      end: moment(currentDate).endOf("quarter").format(DATE_FORMAT),
+    };
+  };
+};
 
 /**
  * factory function (valueProvider)
  * returns a function (which acts as valueProvider and returns last Quarter start and end date)
  * @param {String} fyStartsFrom is String in the format “dd/mm”.
  */
-export const lastQuarter = (fyStartsFrom) => {};
+export const lastQuarter = (fyStartsFrom) => {
+  return () => {
+    return {
+      start: moment(currentDate).subtract(1, "Q").startOf("quarter").format(DATE_FORMAT),
+      end: moment(currentDate).subtract(1, "Q").endOf("quarter").format(DATE_FORMAT),
+    };
+  };
+};
 
 /**
  * factory function (valueProvider)
  * returns a function (which acts as valueProvider and returns this financian year start and end date)
  * @param {String} startsFrom is String in the format “dd/mm”.
  */
-export const thisFinancialYear = (startsFrom) => {};
+export const thisFinancialYear = (startsFrom) => {
+  let startsFromDay = moment(startsFrom, "DD/MM").date();
+  let startsFromMonth = moment(startsFrom, "DD/MM").month();
+  return () => {
+    return {
+      start: moment(currentDate).month(startsFromMonth).date(startsFromDay).format(DATE_FORMAT),
+      end: moment(currentDate)
+        .month(startsFromMonth - 1)
+        .date(startsFromDay)
+        .add(1, "y")
+        .endOf("month")
+        .format(DATE_FORMAT),
+    };
+  };
+};
 
 /**
  * factory function (valueProvider)
  * returns a function (which acts as valueProvider and returns last financian year start and end date)
  * @param {String} startsFrom is String in the format “dd/mm”.
  */
-export const lastFinancialYear = (startsFrom) => {};
+export const lastFinancialYear = (startsFrom) => {
+  let startsFromDay = moment(startsFrom, "DD/MM").date();
+  let startsFromMonth = moment(startsFrom, "DD/MM").month();
+  return () => {
+    return {
+      start: moment(currentDate)
+        .month(startsFromMonth)
+        .date(startsFromDay)
+        .subtract(1, "years")
+        .format(DATE_FORMAT),
+      end: moment(currentDate)
+        .month(startsFromMonth - 1)
+        .date(startsFromDay)
+        .add(1, "y")
+        .endOf("month")
+        .subtract(1, "years")
+        .format(DATE_FORMAT),
+    };
+  };
+};
+
+/**
+ * Computed last Nth Month start and end date (N is a positive integer)
+ * @param {Number} n last nth month
+ * @returns {Function} function (which acts as valueProvider and returns last Nth Month start and end date)
+ */
+export const lastNthMonth = (n) => {
+  return () => {
+    return {
+      start: moment(currentDate).subtract(n, "months").startOf("month").format(DATE_FORMAT),
+      end: moment(currentDate).subtract(n, "months").endOf("month").format(DATE_FORMAT),
+    };
+  };
+};
+
+/**
+ * only the end date is provided.
+ * If n is not a positive integer then return the current day as the end date and throw a warning log.
+ * @param {*} n last n days
+ * @returns {Function} function (which acts as valueProvider and returns before N days (from today)
+ */
+export const beforeNDays = (n) => {
+  return () => {
+    return {
+      end: moment(currentDate).subtract(n, "days").format(DATE_FORMAT),
+    };
+  };
+};
